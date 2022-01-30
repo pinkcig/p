e9abf409fc48ba8e36aa5abfa6dacc276bda8199
Faye Keller <ikurogane85@gmail.com>
2022-01-30 14:43:20 +0100

    feat(presence): add data polling

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ const ACTIVITY = LANYARD_DATA.find((activity: Activity) =>
   [0, 2].includes(activity.type)
 );
 
-const getImage = (activity: Activity) => {
+const getImage = (activity?: Activity) => {
+  if (!activity) return "https://i.stack.imgur.com/y9DpT.jpg";
   if (activity.type === 2)
     return `https://i.scdn.co/image/${
       activity.assets.large_image?.toString().split(":")[1]
@@ -16,17 +17,34 @@ const getImage = (activity: Activity) => {
   return `https://cdn.discordapp.com/app-assets/${activity.application_id}/${activity.assets.large_image}.png`;
 };
 
-const getTitle = (activity: Activity) => {
+const getTitle = (activity?: Activity) => {
+  if (!activity) return "Doing nothing";
   if (activity.type === 2) return `Listening to ${activity.name}`;
   return `Playing ${activity.name}`;
 };
 
-document.querySelector<HTMLImageElement>("#card-image")!.src = ACTIVITY
-  ? getImage(ACTIVITY)
-  : "https://i.stack.imgur.com/y9DpT.jpg";
+const getDescription = (activity?: Activity) => {
+  if (!activity) return "No details.";
+  if (activity.type === 2)
+    return [`By ${activity.state}`, activity.details].join("\n");
+  return `${activity.details}\n${activity.state}`;
+};
 
-const CONTENT = document.querySelector("#card-content")!.children!;
-CONTENT[0].textContent = ACTIVITY ? getTitle(ACTIVITY) : "Doing nothing";
-CONTENT[1].textContent = ACTIVITY
-  ? `${ACTIVITY.details}\n${ACTIVITY.state}`
-  : "No details";
+const updateData = (activity?: Activity) => {
+  document.querySelector("#card-image")?.setAttribute("src", getImage(activity));
+
+  const CONTENT = document.querySelector("#card-content")!.children!;
+  CONTENT[0].textContent = getTitle(activity);
+  CONTENT[1].textContent = getDescription(activity);
+};
+
+updateData(ACTIVITY);
+
+setInterval(async () => {
+  const LANYARD_DATA = (await getLanyardData(USER_ID)).activities;
+  const ACTIVITY = LANYARD_DATA.find((activity: Activity) =>
+    [0, 2].includes(activity.type)
+  );
+
+  updateData(ACTIVITY);
+}, 10e3);
