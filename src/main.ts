fd9dd9cb54a4076cb4def1ab617a19bcba8d213a
Faye Keller <ikurogane85@gmail.com>
2022-01-30 21:09:33 +0100

    feat(card): add bio card, also more pop sounds

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { getLanyardData, updateData } from "./utilities";
 import { Activity } from "./typings";
 import { USER_ID, POP } from "./content";
 
-window.addEventListener("click", () => POP.play());
+document.addEventListener("DOMContentLoaded", () => {
+  POP.play();
+
+  document
+    .querySelectorAll(".card")
+    .forEach((card) => card.addEventListener("mouseenter", () => POP.play()));
+});
 
 getLanyardData(USER_ID).then(
   async (data) =>
