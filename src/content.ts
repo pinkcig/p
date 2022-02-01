242a11d61dc95ab429b7265b2d166ab0e96735d3
Faye Keller <ikurogane85@gmail.com>
2022-02-01 13:59:50 +0100

    chore: remove lanyard

--- src/content.ts
+++ src/content.ts
@@ -1,16 +1,3 @@
-import { IActivityContent } from "./typings";
-
-const USER_ID = "335394597763153920";
 const POP = new Audio("pop.mp3");
-const APPLICATIONS_RESPONSES: Record<string, IActivityContent> = {
-  Spotify: {
-    state: (state?: string) => `By ${state}`,
-    details: (details?: string) => `${details}`,
-  },
-  "Visual Studio Code": {
-    state: (state?: string) => `${state}`,
-    details: (details?: string) => `${details}`,
-  },
-};
 
-export { APPLICATIONS_RESPONSES, USER_ID, POP };
+export { POP };
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,11 @@
 import "./style.css";
-import { getLanyardData, updateData } from "./utilities";
-import { Activity } from "./typings";
-import { USER_ID, POP } from "./content";
+import { POP } from "./content";
 
-document.addEventListener("DOMContentLoaded", () => {
-  POP.play();
+document.addEventListener("DOMContentLoaded", async () => {
+  await POP.play();
 
-  document.addEventListener('click', () => POP.play());
+  document.addEventListener("click", () => POP.play());
   document
     .querySelectorAll(".card")
     .forEach((card) => card.addEventListener("mouseenter", () => POP.play()));
 });
-
-getLanyardData(USER_ID).then(
-  async (data) =>
-    await updateData(
-      data.activities.find((activity: Activity) =>
-        [0, 2].includes(activity.type)
-      )
-    )
-);
-
-setInterval(async () => {
-  const LANYARD_DATA = (await getLanyardData(USER_ID)).activities;
-  const ACTIVITY = LANYARD_DATA.find((activity: Activity) =>
-    [0, 2].includes(activity.type)
-  );
-
-  updateData(ACTIVITY);
-}, 10e3);
