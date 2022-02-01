ec8083b82e3c9322e7acb0087c15c4529625cefc
Faye Keller <ikurogane85@gmail.com>
2022-02-01 14:19:01 +0100

    feat(devtools): no more devtools

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import "./style.css";
 import { POP } from "./content";
 
+// Avoid people opening devtools because I'm a bitch
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+document.addEventListener("keydown", (event) => {
+  if (
+    event.keyCode === 123 ||
+    (event.ctrlKey && event.shiftKey && event.keyCode === 73)
+  )
+    event.preventDefault();
+});
+
+// Wait for DOM to load because it actually matters here, I believe
 document.addEventListener("DOMContentLoaded", async () => {
   await POP.play();
 
